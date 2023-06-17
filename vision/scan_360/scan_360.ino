483eeca6a99a8e149ffcdb8ae389960126d808cc
sebast0209 <sebastiantan.0902@gmail.com>
2023-06-17 19:26:36 +0100

    code for scanning beacons done

---
+++ vision/scan_360/scan_360.ino
@@ -0,0 +1,157 @@
+#include <AccelStepper.h>
+
+#define RX_PIN 16
+#define TX_PIN 17
+
+#define LEFT_STEP_PIN 27 //A4
+#define LEFT_DIR_PIN 15 //D12
+
+#define RIGHT_STEP_PIN 33 //A3
+#define RIGHT_DIR_PIN 4 //D11
+
+#define STEPS_PER_REVOLUTION 200.0
+#define WHEEL_RADIUS 0.0325
+const float wheel_diameter = 2.0 * WHEEL_RADIUS;
+const float wheel_base = 0.14;
+const float wheel_circumference = wheel_diameter * PI;
+uint32_t message;
+
+struct WheelTurns {
+  float l;
+  float r;
+
+  public:
+  WheelTurns(int x_, int y_) {
+    l = x_;
+    r = y_;
+  }
+};
+
+AccelStepper leftStepper(AccelStepper::DRIVER, LEFT_STEP_PIN, LEFT_DIR_PIN);
+AccelStepper rightStepper(AccelStepper::DRIVER, RIGHT_STEP_PIN, RIGHT_DIR_PIN);
+
+WheelTurns red(0,0);
+WheelTurns blue(0,0);
+WheelTurns yellow(0,0);
+
+//functions
+
+bool is_in_frame(int x_min, int x_max, int y_min, int y_max) {
+  return x_min > 240 && x_max <400 && y_min >180 && y_max < 300;
+}
+
+float size_bb(float min_x, float max_x, float min_y, float max_y){
+    float size = (max_x - min_x) * (max_y - min_y);
+    return size;
+}
+  
+
+void setup(){
+  leftStepper.setMaxSpeed(40);
+  leftStepper.setAcceleration(10);
+  rightStepper.setMaxSpeed(40);
+  rightStepper.setAcceleration(10);
+}
+
+void loop(){
+  Serial.begin(115200);
+  Serial1.begin(115200, SERIAL_8N1, RX_PIN, TX_PIN);
+  leftStepper.setCurrentPosition(0);
+  rightStepper.setCurrentPosition(0);
+  uint32_t numbers[14];
+  Serial.println("initialising");
+
+  for (int i = 0; i < 43 ; i++) {
+    Serial.println("entering loop");
+    rightStepper.move(10);
+    leftStepper.move(10);
+
+    if (Serial1.available() >= 4)
+  {
+        byte m1 = Serial1.read();    // read the bytes into byte variables 'b1' to 'b4'
+        byte m2 = Serial1.read();  
+        byte m3 = Serial1.read();
+        byte m4 = Serial1.read();
+
+        uint32_t message;
+        message = m1 | (m2 << 8) | (m3 << 16) | (m4 << 24); 
+
+        if(message == 5390914)
+        {
+            if (Serial1.available() >= 24)
+            {
+                uint32_t temp;
+                uint32_t hexadeci[7];
+                char terminal[7][9];
+                byte b1, b2, b3, b4;
+
+                for (int i = 0; i < 6; i++)
+                {
+                    b1 = Serial1.read(); // read the bytes into byte variables 'b1' to 'b4'
+                    b2 = Serial1.read();
+                    b3 = Serial1.read();
+                    b4 = Serial1.read();
+
+                    hexadeci[i] = b1 | (b2 << 8) | (b3 << 16) | (b4 << 24); // Combine the bytes into a single 32-bit integer
+                    
+                    // char buffer[9];  // Create a character buffer to hold the hexadecimal representation
+                    sprintf(terminal[i], "%08X", hexadeci[i]); // Convert the long value to hexadecimal
+
+                    // To extract bits 10 through 0, we use a bitwise AND with a mask where these bits are 1 and the others are 0.
+                    numbers[2 * i] = hexadeci[i] & 0x7FF;
+
+                    // To extract bits 27 through 16, we again use a bitwise AND, then shift the result right 16 places.
+                    numbers[2 * i + 1] = (hexadeci[i] & 0x0FFF0000) >> 16;
+                }
+                bool red_detect = is_in_frame(numbers[0], numbers[2],numbers[1], numbers[3]);
+                bool blue_detect = is_in_frame(numbers[4], numbers[6],numbers[5], numbers[7]);
+                bool yellow_detect = is_in_frame(numbers[8], numbers[10],numbers[9], numbers[11]);
+
+                int left_wheel_revs = leftStepper.currentPosition();
+                int right_wheel_revs = rightStepper.currentPosition();
+
+                if (red_detect || yellow_detect) {
+                  float red_size = size_bb(numbers[0], numbers[2],numbers[1], numbers[3]);
+                  float yellow_size = size_bb(numbers[8], numbers[10],numbers[9], numbers[11]);
+                  
+                  if (yellow_size > 0.5*red_size) {
+                    yellow.l = left_wheel_revs;
+                    yellow.r = right_wheel_revs;
+                    Serial.println("yellow detected!!");
+                    Serial.println("left wheel revs = " + String(yellow.l));
+                    Serial.println("right wheel revs = " + String(yellow.r));
+                  }
+                  else {
+                    red.l = left_wheel_revs;
+                    red.r = right_wheel_revs;
+                    Serial.println("red detected!!");
+                    Serial.println("left wheel revs = " + String(red.l));
+                    Serial.println("right wheel revs = " + String(red.r));
+                  }
+                  
+                }
+
+                else if (blue_detect) {
+                  blue.l = left_wheel_revs;
+                  blue.r = right_wheel_revs;
+                  Serial.println("yellow detected!!");
+                  Serial.println("left wheel revs = " + String(blue.l));
+                  Serial.println("right wheel revs = " + String(blue.r));
+                }
+                
+            }
+        }
+
+        Serial.println("done turning one turn!");
+   }
+
+  Serial.println(numbers[0]);
+    
+  while(rightStepper.distanceToGo() != 0 && leftStepper.distanceToGo() != 0) {
+    rightStepper.run();
+    leftStepper.run();
+  }
+  }
+  Serial.println("done turning!");
+  
+}
