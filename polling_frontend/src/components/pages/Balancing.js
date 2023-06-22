123111a78bf93faf995db6281eb291b58d2a33d0
j-k-quinn <johanna.quinn21@imperial.ac.uk>
2023-06-22 18:25:19 +0100

    changes to design

---
+++ polling_frontend/src/components/pages/Balancing.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../../App.css';
+// import video1 from "../../video.mp4";
+// import './Balancing.css'
+
+function Balancing() {
+
+  return (
+    <div className='video-container'>
+      <h1 className='Balancing'>WATCH THE ROVER BALANCING</h1>
+      <div className='video-wrapper'>
+        <video className='centered-video' controls='controls' autoPlay={true} >
+          <source src='../../video.mp4' type='video/mp4' />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+    </div>
+    );
+}
+export default Balancing;
